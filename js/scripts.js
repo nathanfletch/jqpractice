c514d3e6d37210e02efc6d5632c1dffd99a738e1
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-08-10 17:22:45 -0700

    Add lorem content generation

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,44 @@
 $(document).ready(function() {
-  $("#clickShow").click(function() {
-    $("#img-showing").fadeIn();
-    $("#img-hidden").fadeOut();
+  $(".clickable").click(function() {
+    $(".walrus-show").slideToggle();
+    $(".walrus-hidden").slideToggle();
   });
-  $("#clickHide").click(function() {
-    $("#img-showing").slideUp();
-    $("#img-hidden").slideDown();
+
+  $(".toggle-text").click(function() {
+    $(".text-showing").slideToggle();
+  });
+
+  $("button#green").click(function() {
+    $("body").removeClass("yellow-background red-background dark-mode");
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass("green-background red-background dark-mode");
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass("green-background yellow-background dark-mode");
+    $("body").addClass("red-background");
+  });
+
+  $("button#dark").click(function() {
+    
+    $("body").removeClass("green-background yellow-background red-background");
+    $("body").toggleClass("dark-mode");
+  });
+
+  $("button#borders").click(function() {
+    $(".expandable").toggleClass("border");
+  });
+
+  $("button#random").click(function() {
+    // $("#content").prepend("<div><p>sometext</p></div>");
+
+    $("#content").prepend("<div class='expandable'>    <div class='bg-secondary'>      <p class='toggle-text'>Lorem ipsum dolor sit amet.</p>    </div>    <div class='text-showing'>      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,      quo. Sit repellendus tempore animi eum quos eveniet? Officia,      perferendis ea.    </div>  </div>");
+    $(this).children("div").first().click(function() {
+      $(this).children("div").first().slideToggle();
+    });
   });
 });
